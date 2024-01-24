d4fe14f29d95afe0a43a0f4484d21d10a11a29e8
oALOKo <154063795+oALOKo@users.noreply.github.com>
2024-01-24 22:14:37 +0530

    Update doom.js

--- doom.js
+++ doom.js
@@ -48,7 +48,7 @@ setTimeout(()=>{
     console.log("Score saved")
     })
 
-    const redirectTo = 'https://oaloko.github.io/Leaderboards/'
+    const redirectTo = './LeaderBoards.html?username=' + encodeURIComponent(decodedUsername);
         setTimeout(() => {
           window.location.replace(redirectTo); 
       },3000);   
